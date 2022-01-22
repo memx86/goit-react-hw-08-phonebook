afc0be011588d2e634f5f8f202d77d8595ddf37d
Yevhen Malyshko <memx86@gmail.com>
2022-01-22 22:13:49 +0200

    initial files and project structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import {Component, Fragment } from "react";
-import Section from "Components/Section";
-import Container from "Components/Container";
+import { Component, Fragment } from "react";
+// import Section from "Components/Section";
+// import Container from "Components/Container";
 
 class App extends Component {
   state = {
     contacts: [],
-    filter: ''
-  }
-  
-  render() {
-    return (
-      <Fragment>
+    filter: "",
+  };
 
-      </Fragment>
-    );
-    }
+  render() {
+    return <Fragment></Fragment>;
+  }
 }
 export default App;
