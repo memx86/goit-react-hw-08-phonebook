9778df88ba2da98650cbe43838bbf42d90ff6195
Yevhen Malyshko <memx86@gmail.com>
2022-01-28 16:45:57 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component, Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import Section from "components/Section";
@@ -7,81 +7,69 @@ import ContactForm from "components/ContactForm";
 import Filter from "components/Filter";
 import ContactList from "components/ContactList";
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: "",
-  };
-  componentDidMount() {
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState("");
+  useEffect(() => {
     try {
       const localContacts = JSON.parse(localStorage.getItem("contacts"));
-      if (localContacts) this.setState({ contacts: localContacts });
+      if (localContacts) setContacts(localContacts);
     } catch {}
-  }
-  componentDidUpdate(prevProps, prevState) {
-    const prevContacts = prevState.contacts;
-    const contacts = this.state.contacts;
-    if (prevContacts !== contacts) {
-      localStorage.setItem("contacts", JSON.stringify(contacts));
-    }
-  }
-  addContact = (contact) => {
-    this.setState(({ contacts }) => ({ contacts: [...contacts, contact] }));
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+  const addContact = (contact) => {
+    setContacts((contacts) => [...contacts, contact]);
   };
-  removeContact = (e) => {
+  const removeContact = (e) => {
     const name = e.target.dataset.name;
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((contact) => contact.name !== name),
-    }));
+    setContacts((contacts) =>
+      contacts.filter((contact) => contact.name !== name)
+    );
     toast.info(`${name} was removed from contacts`);
   };
-  getNames = () => this.state.contacts.map((contact) => contact.name);
-  setFilterState = (value) => this.setState(() => ({ filter: value }));
-  filterContacts = () => {
-    const contacts = this.state.contacts;
-    const filter = this.state.filter.toLowerCase().trim();
+  const getNames = () => contacts.map((contact) => contact.name);
+  const filterContacts = () => {
+    const filterValue = filter.toLowerCase().trim();
     return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter)
+      contact.name.toLowerCase().includes(filterValue)
     );
   };
-  render() {
-    const contacts = this.filterContacts();
-    const names = this.getNames();
-    return (
-      <Fragment>
-        <Section>
-          <Container>
-            <h1>Phonebook</h1>
-          </Container>
-        </Section>
-        <Section>
-          <Container>
-            <ContactForm names={names} onFormSubmit={this.addContact} />
-          </Container>
-        </Section>
-        <Section>
-          <Container>
-            <h2>Contacts</h2>
-            <Filter handleFilter={this.setFilterState} />
-            <ContactList
-              contacts={contacts}
-              removeContact={this.removeContact}
-            />
-          </Container>
-        </Section>
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={true}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </Fragment>
-    );
-  }
+  const finalContacts = filterContacts();
+  const names = getNames();
+  return (
+    <Fragment>
+      <Section>
+        <Container>
+          <h1>Phonebook</h1>
+        </Container>
+      </Section>
+      <Section>
+        <Container>
+          <ContactForm names={names} onFormSubmit={addContact} />
+        </Container>
+      </Section>
+      <Section>
+        <Container>
+          <h2>Contacts</h2>
+          <Filter handleFilter={setFilter} />
+          <ContactList contacts={finalContacts} removeContact={removeContact} />
+        </Container>
+      </Section>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Fragment>
+  );
 }
+
 export default App;
