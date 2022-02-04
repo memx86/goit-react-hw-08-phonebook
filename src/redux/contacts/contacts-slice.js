f6700497ea3caf52702f6384704d20f49daec6c9
Yevhen Malyshko <memx86@gmail.com>
2022-02-04 22:48:17 +0200

    how to change state using immer in contacts-slice

--- src/redux/contacts/contacts-slice.js
+++ src/redux/contacts/contacts-slice.js
@@ -10,14 +10,17 @@ export const contactsSlice = createSlice({
       ...state,
       items: [...state.items, payload],
     }),
+    //immer: state.items = [...state.items, payload];
     remove: (state, { payload }) => ({
       ...state,
       items: state.items.filter((contact) => contact.name !== payload),
     }),
+    //immer: state.items = state.items.filter((contact) => contact.name !== payload);
     changeFilter: (state, { payload }) => ({
       ...state,
       filter: payload,
     }),
+    //immer: state.filter = payload;
   },
 });
 
