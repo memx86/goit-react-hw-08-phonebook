0bb218743f8949e2130925c6dbbe50690070a11c
Yevhen Malyshko <memx86@gmail.com>
2022-02-06 15:32:55 +0200

    selectors memoization

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,17 +1,18 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const getContacts = (state) => state.contacts.items;
 export const getFilter = (state) => state.contacts.filter;
 
-export const getNames = (state) => {
-  const contacts = getContacts(state);
-  return contacts.map((contact) => contact.name);
-};
+export const getNames = createSelector([getContacts], (contacts) =>
+  contacts.map((contact) => contact.name)
+);
 
-export const filterContacts = (state) => {
-  const contacts = getContacts(state);
-  const filter = getFilter(state);
-  if (!filter) return contacts;
-  const filterValue = filter.toLowerCase().trim();
-  return contacts.filter(({ name }) =>
-    name.toLowerCase().includes(filterValue)
-  );
-};
+export const filterContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
+    const filterValue = filter.toLowerCase().trim();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filterValue)
+    );
+  }
+);
