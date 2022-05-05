4932a9c9d1d78c535f71feb5edba353e642ad844
Yevhen Malyshko <memx86@gmail.com>
2022-05-05 11:36:31 +0300

    home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,30 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getIsLoggedIn } from "redux/auth";
+import { useRefreshQuery } from "redux/contacts";
 import Section from "components/Section";
 import Container from "components/Container";
+import AuthForm from "components/AuthForm";
+
 function Home() {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+  const { data } = useRefreshQuery(null, { skip: !isLoggedIn });
   return (
     <Section>
-      <Container>Welcome to Contact Book!</Container>
+      <Container>
+        <h1>Welcome to Contact Book!</h1>
+        <div className="grid">
+          {!isLoggedIn && (
+            <Fragment>
+              <p>Create an account or log in to use app</p>
+              <AuthForm type="Signup" />
+              <Link to="/login">Already have an account?</Link>
+            </Fragment>
+          )}
+          {isLoggedIn && <p>Hi, {data?.name}, glad to see you</p>}
+        </div>
+      </Container>
     </Section>
   );
 }
