61b9ef9a1a7ab7b3c8c387decf61ae28a6d46b57
Yevhen Malyshko <memx86@gmail.com>
2022-05-08 22:20:57 +0300

    netlify

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
